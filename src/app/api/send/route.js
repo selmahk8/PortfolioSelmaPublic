6e041513fc51008e31e62f88b2dc78a10b781a11
Selma Hadj Khelifa <77730138+selmahk8@users.noreply.github.com>
2023-11-11 23:15:29 +0100

    route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend('re_B8JzGQgx_JgeLjR2ttW9jw1kgnZPKVLzR');
+const resend = new Resend('your_resend_code_here');
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
@@ -10,12 +10,12 @@ export async function POST(req, res) {
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: "selmahk@resend.dev",
-      to: ["selmahadjkhelifa@yahoo.fr", email],
+      from: "your_name_here@resend.dev",
+      to: ["your_email_here", email],
       subject: subject,
       react: (
         <>
-          <h1>Thank you for contacting Selma!</h1>
+          <h1>Thank you for contacting Me !</h1>
           <p>{subject}</p>
           <h1>New message submitted:</h1>
           <p>{message}</p>
