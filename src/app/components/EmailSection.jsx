0664866305d5e7c1b42a66a914af48d5ae6b156f
Selma Hadj Khelifa <77730138+selmahk8@users.noreply.github.com>
2023-11-11 23:11:45 +0100

    EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/selmahk8">
+          <Link href="https://github.com/">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="https://www.linkedin.com/in/selmahadjkhelifa/">
+          <Link href="https://www.linkedin.com/in/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
